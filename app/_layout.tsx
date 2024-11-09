add658966601a924c37054decaa72854677b0b84
thezeeshann <info.zeeshann@gmail.com>
2024-11-09 19:07:58 +0530

    update components

---
+++ app/_layout.tsx
@@ -0,0 +1,31 @@
+import { Tabs } from "expo-router";
+import Entypo from "@expo/vector-icons/Entypo";
+import Feather from "@expo/vector-icons/Feather";
+import { theme } from "@/theme";
+
+export default function Layout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorGreen }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarShowLabel: false,
+          tabBarIcon: ({ size, color }) => (
+            <Entypo name="leaf" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarShowLabel: false,
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="user" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
